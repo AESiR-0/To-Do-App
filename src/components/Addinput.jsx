5c15b849ccc35a64ecb40c7ad39d3359a0eee581
AESiR <cyborgkiller1008@gmail.com>
2023-05-20 00:25:35 +0530

    added input ui

--- src/components/Addinput.jsx
+++ src/components/Addinput.jsx
@@ -1,9 +1,11 @@
-
+import { FaPlus } from "react-icons/fa";
 function Addinput() {
   return (
-    <div>
+    <div className="">
       <div className="flex justify-center align-items">
-      <input type="text" className="yes" />
+      <input type="text" placeholder="Title" className="Name" />
+      <input type="text" placeholder="Descirption" className="descirption" />
+      <button className="plus-button" id="add task"><FaPlus/></button>
       </div>
     </div>
   )
