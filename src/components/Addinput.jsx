a4f8682547e606c7275c46a4da1d0a345cb23065
AESiR <cyborgkiller1008@gmail.com>
2023-05-20 16:17:07 +0530

    adding logic

--- src/components/Addinput.jsx
+++ src/components/Addinput.jsx
@@ -1,14 +1,58 @@
 import { FaPlus } from "react-icons/fa";
-function Addinput() {
+import { useRef, useState } from "react";
+
+export default function Addinput() {
+  const inputTitle = useRef("");
+  const inputDescription = useRef("");
+  const [data, setData] = useState([]);
+
   return (
     <div className="">
       <div className="flex justify-center align-items">
-      <input type="text" placeholder="Title" className="Name" />
-      <input type="text" placeholder="Descirption" className="descirption" />
-      <button className="plus-button" id="add task"><FaPlus/></button>
+        <input
+          type="text"
+          placeholder="Title"
+          className="Name"
+          ref={inputTitle}
+        />
+        <input
+          type="text"
+          placeholder="Descirption"
+          className="descirption"
+          ref={inputDescription}
+        />
+        <button
+          className="plus-button"
+          onClick={(e) => {
+            e.preventDefault();
+            setData([
+              ...data,
+              {
+                title: inputTitle.current.value,
+                description: inputDescription.current.value,
+              },
+            ]);
+            inputTitle.current.value = '',
+            inputDescription.current.value = ''
+          }}
+
+
+          id="add task"
+        >
+          <FaPlus />
+        </button>
+      </div>
+      <div className="preview grid grid-cols-5 h-full w-full">
+          {data && data.length>0 ? data.map((value, index)=>{
+            return(
+              <div key={index}>
+                
+              <h2>{value.title}</h2>
+              </div>
+            )
+          }): "No ToDo"}
+        
       </div>
     </div>
-  )
+  );
 }
-
-export default Addinput
--- src/components/Dark.jsx
+++ src/components/Dark.jsx
@@ -6,8 +6,8 @@ const ehTheme = ()=> {theme==="light" ? setTheme('dark'): setTheme('light')};
 const data = ()=> childToParent(theme);
   return (
     <div className='theme-swtich'>
-      <button className='theme-switch' onClick={()=>{ehTheme();data();}}>
-        <img src="darkmode.png" alt="" />
+      <button className='theme-switch mix-blend-color-burn' onClick={()=>{ehTheme();data();}}>
+        <img className="mix-blend-color-burn" src="darkmode.png" alt="" />
       </button>
     </div>
   )
